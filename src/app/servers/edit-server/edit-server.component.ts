afd186bf5e2aa6e8d1e4504d8137f43041307e55
= <=>
2018-03-03 05:12:09 -0600

    First commit of CLI files

---
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ServersService } from '../servers.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-edit-server',
+  templateUrl: './edit-server.component.html',
+  styleUrls: ['./edit-server.component.css']
+})
+export class EditServerComponent implements OnInit {
+  server: {id: number, name: string, status: string};
+  serverName = '';
+  serverStatus = '';
+
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    // Not reactive. If url changes while component is active, the change
+    // will not he reponded to.
+    console.log(this.route.snapshot.queryParams);
+    console.log(this.route.snapshot.fragment);
+    // To keep track of changes, subscribe to the observable properties
+    // of the ActivatedRoute object. NOTE we did not add functions to react 
+    // to changes here.
+    this.route.queryParams.subscribe();
+    this.route.fragment.subscribe();
+    
+    this.server = this.serversService.getServer(1);
+    this.serverName = this.server.name;
+    this.serverStatus = this.server.status;
+  }
+
+  onUpdateServer() {
+    this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+  }
+
+}
